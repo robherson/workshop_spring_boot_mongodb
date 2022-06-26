afa70b3c0b4c89619797835a14dc8b69af4f876b
Robherson Wector <robhersonwector@gmail.com>
2022-06-26 10:45:23 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/robherson/workshopmongo/domain/User.java
@@ -0,0 +1,67 @@
+package com.robherson.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable{
+
+    private String id;
+    private String name;
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(String id, String name, String email){
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+
+}
